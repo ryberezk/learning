c2808de73a49b189be14a58cd7ba036e0c2f6c90
ryberezk@gmail.com <!FVGTHCFYn0>
2018-06-13 17:26:38 +0300

    Расстояния до точек

--- src/main/java/my/project/pack/MyFistProgram.java
+++ src/main/java/my/project/pack/MyFistProgram.java
@@ -16,5 +16,17 @@ public class MyFistProgram {
 
         t.area();
         System.out.println(t);
+        Point point3 = new Point(1.2,1.6);
+        Point point4 = new Point(1.9,1.8);
+        Point point1 = new Point(2.9,1.2);
+        Point point2 = new Point(6.9,3.4);
+        System.out.println("Расстояние от точки "+point4+" до точки " +point1 + " равно "+point1.distanceto(point4));
+        System.out.println("Расстояние равно: " + distance(point3,point4));
+
     }
+
+    public static double distance (Point p1, Point p2){
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2)+Math.pow((p2.y - p2.y), 2));
+    }
+
 }
